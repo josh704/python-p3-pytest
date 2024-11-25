36693fbd7be43a9d39e9c6c17f7d28f198031c46
josh704 <joshuangetich02@gmail.com>
2024-11-25 22:29:18 +0300

    python-p3-pytest

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from not_none_functions import return_not_none
+def test_return_not_none():
+    '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+    from not_none_functions import return_not_none
 
 # def test_return_not_none():
 #     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
